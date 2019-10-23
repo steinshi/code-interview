4ac0b0ff3155253634934df17e72dd1382e91755
yuvalpeled <yuval@getvim.com>
2019-10-23 11:14:08 +0300

    Cleaner tests that don't change the state of the tested servers Added "mocha test" to test both parts of the coding interview

--- test/tests/test-b.spec.js
+++ test/tests/test-b.spec.js
@@ -28,30 +28,47 @@ function wait(ms) {
     })
 }
 
+const supermanProvider = {
+    name: "Clark Kent",
+    score: 1000,
+    specialties: [
+        "Supertreatment"
+    ],
+    availableDates: [
+        {
+            gte: "1995-10-20T08:00+0100",
+            lte: "1995-10-20T08:00+0100"
+        }
+    ]
+};
+
+function addSuperman() {
+    const specialty = supermanProvider.specialties[0];
+    const date = supermanProvider.availableDates[0].lte;
+    return publishMessage(`addProvider`, supermanProvider)
+        .then(() => wait(200))
+        .then(() => getAppointments(specialty, date))
+        .then(result => assert.deepStrictEqual(result, [supermanProvider.name]));
+}
+
+function removeSuperman() {
+    const specialty = supermanProvider.specialties[0];
+    const date = supermanProvider.availableDates[0].lte;
+    return publishMessage(`deleteProvider`, {name: supermanProvider.name})
+        .then(() => wait(200))
+        .then(() => getAppointments(specialty, date))
+        .then(result => assert.deepStrictEqual(result, []));
+}
+
+
 describe(`# Test part B of the coding interview`, () => {
     describe(`# Test addProvider`, () => {
-        it(`# Should add a provider`, () => {
-            const providerToAdd = {
-                name: "Clark Kent",
-                score: 1000,
-                specialties: [
-                    "Supertreatment"
-                ],
-                availableDates: [
-                    {
-                        gte: "2050-01-01T08:00+0100",
-                        lte: "2050-01-01T10:00+0100"
-                    }
-                ]
-            };
-            return publishMessage(`addProvider`, providerToAdd)
-                .then(() => wait(500))
-                .then(() => getAppointments(`Supertreatment`, `2050-01-01T08:00+0100`))
-                .then(result => assert.deepStrictEqual(result, ['Clark Kent']))
-        });
+        afterEach(removeSuperman);
+
+        it(`# Should add a provider`, addSuperman);
 
         it(`# Should update a provider`, () => {
-            const providerToAdd = {
+            const differentSuperman = {
                 name: "Clark Kent",
                 score: 1000,
                 specialties: [
@@ -64,48 +81,66 @@ describe(`# Test part B of the coding interview`, () => {
                     }
                 ]
             };
-            return publishMessage(`addProvider`, providerToAdd)
-                .then(() => wait(300))
-                .then(() => getAppointments(`Supertreatment`, `2050-01-01T08:00+0100`))
+            return addSuperman()
+                .then(() => publishMessage(`addProvider`, differentSuperman))
+                .then(() => wait(200))
+                .then(() => getAppointments(supermanProvider.specialties[0], supermanProvider.availableDates[0].lte))
                 .then(result => assert.deepStrictEqual(result, []))
-                .then(() => getAppointments(`Measles`, `2050-01-01T08:00+0100`))
-                .then(result => assert.deepStrictEqual(result, ["Clark Kent"]))
+                .then(() => getAppointments(differentSuperman.specialties[0], differentSuperman.availableDates[0].lte))
+                .then(result => assert.deepStrictEqual(result, [differentSuperman.name]));
         })
     });
 
     describe(`# Test deleteProvider`, () => {
+        // Make sure we have a provider to delete
+        before(addSuperman);
+
+        // Make sure the provider is deleted no matter what happened in the test
+        after(removeSuperman);
+
         it(`# Should delete a provider`, () => {
-            const providerToDelete = {name: 'Randall Flagg', specialty: 'Physiologist', date: '2027-04-29T08:00+0100'};
-            return getAppointments(providerToDelete.specialty, providerToDelete.date)
-                .then(result => assert.deepStrictEqual(result, [providerToDelete.name]))
-                .then(() => publishMessage(`deleteProvider`, {name: providerToDelete.name}))
-                .then(() => wait(300))
-                .then(() => getAppointments(providerToDelete.specialty, providerToDelete.date))
+            // Before deleting, add a provider to delete
+            return publishMessage(`deleteProvider`, {name: supermanProvider.name})
+                .then(() => wait(200))
+                .then(() => getAppointments(supermanProvider.specialties[0], supermanProvider.availableDates[0].lte))
                 .then(result => assert.deepStrictEqual(result, []));
         });
-    })
+    });
 
     describe(`# Test updateTimeslots`, () => {
+        // Make sure we have a provider to update
+        before(addSuperman);
+
+        //cleanup;
+        after(removeSuperman);
+
         it(`# Should update available dates`, () => {
-            const timeBeforeUpdate = "1995-10-20T08:00+0100";
+            const timeBeforeUpdate = supermanProvider.availableDates[0].lte;
             const timeAfterUpdate = "1990-10-20T08:00+0100";
             const providerToUpdate = {
-                name: 'Oy Midworld', availableDates: [
+                name: supermanProvider.name, availableDates: [
                     {
                         gte: timeAfterUpdate,
                         lte: timeAfterUpdate
                     }
                 ]
             };
-            return getAppointments('Neonatal', timeBeforeUpdate)
-                .then(result => assert.deepStrictEqual(result, [providerToUpdate.name]))
-                .then(() => getAppointments('Neonatal', timeAfterUpdate))
+            const specialty = supermanProvider.specialties[0];
+
+            // First validate the existence of the provider before the update
+            return getAppointments(specialty, timeBeforeUpdate)
+                .then(result => assert.deepStrictEqual(result, [supermanProvider.name]))
+                .then(() => getAppointments(specialty, timeAfterUpdate))
                 .then(result => assert.deepStrictEqual(result, []))
-                .then(() => publishMessage(`updateTimeslots`, providerToUpdate))
+
+                // Make the update
+                .then(() => publishMessage('updateTimeslots', providerToUpdate))
                 .then(() => wait(300))
-                .then(() => getAppointments('Neonatal', timeBeforeUpdate))
+
+                // Make sure the provider was changed
+                .then(() => getAppointments(specialty, timeBeforeUpdate))
                 .then(result => assert.deepStrictEqual(result, []))
-                .then(() => getAppointments('Neonatal', timeAfterUpdate))
+                .then(() => getAppointments(specialty, timeAfterUpdate))
                 .then(result => assert.deepStrictEqual(result, [providerToUpdate.name]))
         });
     })
